0f590c7658962ee23187c2c9cdd210049567cd7b
JordanDev26 <jrescalantedev@gmail.com>
2023-10-12 22:31:26 -0600

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import org.jrescalante.supermarketbag.model.Clean;
+import org.jrescalante.supermarketbag.model.Dairy;
+import org.jrescalante.supermarketbag.model.SuperMarketBag;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SuperMarketBag<Clean> cleaningBag = new SuperMarketBag<>();
+        cleaningBag.addProduct(new Clean("Escoba",100.0,"Palo, Escoba",0))
+                .addProduct(new Clean("Fabuloso",100.0,"Quimico, fresa",2))
+                .addProduct(new Clean("Escoba",100.0,"Palo, Escoba",0))
+                .addProduct(new Clean("Fabuloso",100.0,"Quimico, fresa",2));
+
+        for (Clean c: cleaningBag){
+            System.out.println(c.getName());
+            System.out.println(c.getPrice());
+            System.out.println(c.getComponents());
+            System.out.println(c.getLiters());
+        }
+
+        SuperMarketBag<Dairy> dairyBag = new SuperMarketBag<>();
+        dairyBag.addProduct(new Dairy("Milk",200.0,5,7));
+        dairyBag.addProduct(new Dairy("Yogurt",200.0,5,7));
+        dairyBag.addProduct(new Dairy("Protein",200.0,5,7));
+        dairyBag.addProduct(new Dairy("Trembo",200.0,5,7));
+
+        for (Dairy dairy: dairyBag){
+            System.out.println(dairy.getName());
+            System.out.println(dairy.getPrice());
+            System.out.println(dairy.getAmount());
+            System.out.println(dairy.getProtein());
+        }
+
     }
 }
--- src/org/jrescalante/supermarketbag/model/SuperMarketBag.java
+++ src/org/jrescalante/supermarketbag/model/SuperMarketBag.java
@@ -1,20 +1,26 @@
 package org.jrescalante.supermarketbag.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class SuperMarketBag<T> {
+public class SuperMarketBag<T> implements Iterable<T> {
     List<T> products;
     public SuperMarketBag(){
         products = new ArrayList<>();
     }
 
-    public void addProduct(T t){
+    public SuperMarketBag<T> addProduct(T t){
         products.add(t);
-
+        return this;
     }
 
     public List<T> getProducts(){
         return products;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return products.iterator();
     }
 }
